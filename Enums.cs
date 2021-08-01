31b24264fc6ffb0d577ca2a7a35e7ae730b90af3
An Bil <anbil0123456789@gmail.com>
2021-08-01 21:06:06 +0200

    Add Enums

---
+++ Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomiaryGUI
+{
+    enum FormStates
+    {
+        start,
+        power, current, voltage, cos, equipments,
+        daily, weekly, monthly, annual,
+        settings
+    }
+
+    enum Raport
+    {
+        day, week, month, year
+    }
+}
